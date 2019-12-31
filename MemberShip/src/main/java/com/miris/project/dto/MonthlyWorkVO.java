eb7b3b63689cc8debb882d60b7abbf65fc047f6c
sosimvoy <sosimvoy@mirisHan>
2019-12-31 18:13:14 +0900

    dto 추가 작업

---
+++ MemberShip/src/main/java/com/miris/project/dto/MonthlyWorkVO.java
@@ -0,0 +1,116 @@
+package com.miris.project.dto;
+
+import java.util.Date;
+
+public class MonthlyWorkVO {
+	
+	//월별 근무이력
+	private String m_id;
+	private String m_year;
+	private String m_month;
+	private String m_work;
+	//private String l_code;
+	private String m_status;
+	private String m_except;
+	private String m_notice;
+	
+	//사이트
+	private String l_code;
+	private String l_name;
+	
+	//조회하기 위한
+	private String m_pw;
+	private String m_name;
+	private String m_gubun;
+	private String d_code;
+	private String m_position;
+	
+	
+	public String getM_id() {
+		return m_id;
+	}
+	public void setM_id(String m_id) {
+		this.m_id = m_id;
+	}
+	public String getM_year() {
+		return m_year;
+	}
+	public void setM_year(String m_year) {
+		this.m_year = m_year;
+	}
+	public String getM_month() {
+		return m_month;
+	}
+	public void setM_month(String m_month) {
+		this.m_month = m_month;
+	}
+	public String getM_work() {
+		return m_work;
+	}
+	public void setM_work(String m_work) {
+		this.m_work = m_work;
+	}
+	public String getM_status() {
+		return m_status;
+	}
+	public void setM_status(String m_status) {
+		this.m_status = m_status;
+	}
+	public String getM_except() {
+		return m_except;
+	}
+	public void setM_except(String m_except) {
+		this.m_except = m_except;
+	}
+	public String getM_notice() {
+		return m_notice;
+	}
+	public void setM_notice(String m_notice) {
+		this.m_notice = m_notice;
+	}
+	public String getL_code() {
+		return l_code;
+	}
+	public void setL_code(String l_code) {
+		this.l_code = l_code;
+	}
+	public String getL_name() {
+		return l_name;
+	}
+	public void setL_name(String l_name) {
+		this.l_name = l_name;
+	}
+	public String getM_pw() {
+		return m_pw;
+	}
+	public void setM_pw(String m_pw) {
+		this.m_pw = m_pw;
+	}
+	public String getM_name() {
+		return m_name;
+	}
+	public void setM_name(String m_name) {
+		this.m_name = m_name;
+	}
+	public String getM_gubun() {
+		return m_gubun;
+	}
+	public void setM_gubun(String m_gubun) {
+		this.m_gubun = m_gubun;
+	}
+	public String getD_code() {
+		return d_code;
+	}
+	public void setD_code(String d_code) {
+		this.d_code = d_code;
+	}
+	public String getM_position() {
+		return m_position;
+	}
+	public void setM_position(String m_position) {
+		this.m_position = m_position;
+	}
+
+	
+	
+}
